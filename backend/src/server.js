48d3eade870427b844276ccef69cff53bd0d4b94
Harish K <harishthebot123@gmail.com>
2025-12-12 00:58:09 +0530

    devv deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,12 +14,15 @@ import gpmsgRouter from './routes/grpmsg.routes.js'
 import path from 'path'
 const __dirname=path.resolve()
 
-if(process.env.NODE_ENV!=='production'){
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://fleet-app-3.onrender.com"    
+];
+
 app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true
-}))
-}
+  origin: allowedOrigins,
+  credentials: true
+}));
 app.use(express.json({limit:"20mb"}))
 app.use(cookieParser())
 
